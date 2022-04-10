733e6cf8acde7ce6e82f4dc63d1e7f5a978f55c2
rodrigoraiman <rodrigoraiman@gmail.com>
2022-04-10 01:41:49 -0400

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+30.times do
+    Customer.create!({
+        name: Faker::Name.name_with_middle
+    })
+
+end
